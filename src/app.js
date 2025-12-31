6ad59f475c1c63adb02e47b9f3e5998c6170357b
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2025-12-31 12:03:25 +0530

    exploring get post delete methods

--- src/app.js
+++ src/app.js
@@ -2,13 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("this is test");
+app.get("/user", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, messge: "Successfully retrived user details" });
 });
 
-app.use("/hello", (req, res) => {
-  res.send("this is hello");
+app.post("/user", (req, res) => {
+  res
+    .status(201)
+    .json({ success: true, messge: "Successfully added new user" });
 });
+
+app.delete("/user", (req, res) => {
+  res.status(200).json({ success: true, messge: "Successfully delted user" });
+});
+
 app.listen(3000, () => {
   console.log("Server is up and running");
 });
